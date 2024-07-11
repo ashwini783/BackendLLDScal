ae1d442fa7e06510298b14f3f9ee59273ea4bcea
Ashwini <ashwiniavhad8@gmail.com>
2024-07-12 00:40:39 +0530

    Lambda and Stream

---
+++ src/LambdaAndStream/Student.java
@@ -0,0 +1,14 @@
+package LambdaAndStream;
+
+public class Student {
+    int age;
+    double psp;
+    String name;
+    double attendance;
+    public Student(int age, double psp, String name, double attendance) {
+        this.age = age;
+        this.psp = psp;
+        this.name = name;
+        this.attendance = attendance;
+    }
+}
