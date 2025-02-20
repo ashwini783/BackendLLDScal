2615264751f4b636d05d7b1d9044234cd9c49d17
Ashwini <ashwiniavhad8@gmail.com>
2025-02-20 11:28:09 +0530

    new changes

--- src/TicTacToe/Models/HumanPlayer.java
+++ src/TicTacToe/Models/HumanPlayer.java
@@ -3,6 +3,8 @@ package TicTacToe.Models;
 public class HumanPlayer extends Player{
 
     public HumanPlayer(int id, String name, PlayerType playerType, Symbol symbol) {
+
         super(id, name, playerType, symbol);
+
     }
 }
